0235009c9c31197e890707001070342f4809fa86
John Gorriceta <johngorriceta@johns-air.mynetworksettings.com>
2023-11-16 00:19:49 -0700

    view profile

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -5,6 +5,7 @@ import { useAtom } from 'jotai';
 import { uuidAtom } from "@/atoms/uuidAtom";
 import { useLayoutEffect } from "react";
 import { useRouter} from "next/navigation";
+import Link from 'next/link';
 
 
 
@@ -58,9 +59,10 @@ export default function Chat() {
           </ul>
       </nav>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className='flex flex-col gap-5 items-center'>
+        <div className='flex flex-col gap-5 items-center text-center'>
           <img src='./Jeff.png' className='h-[100px] w-[100px] rounded-[200px]'/>
-          <h1 className='text-2xl font-bold'>Send a Message to Jeff</h1>
+          <h1 className='text-2xl font-bold'>Connect with Jeff Lazlo</h1>
+          <Link href="https://www.linkedin.com/in/jeff-lazlo-831b6729b/" target='blank'><p className='text-xl text-blue-500 underline'>View Profile</p></Link>
         </div>
         <form onSubmit={(e)=>{sendData(e)}} className='p-3'>
           <textarea onChange={e=>setUserInput(e.target.value)} className="flex border-solid border-2 p-2 border-gray-300 rounded-lg w-[300px] md:w-[400px] h-[200px] resize-none" maxLength={300} placeholder='Ex: We know each other from...'/> 
